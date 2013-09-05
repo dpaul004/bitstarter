f42869b364c8f9aea8cd930a13a01abc7dbeed5c
Ubuntu <ubuntu@ip-172-31-28-225.us-west-2.compute.internal>
2013-09-05 16:21:48 +0000

    indexfix

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
   response.send(readfile(input_file));
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
